5839443a2b8652d7435d71e5a95658a76a211388
David Fuentes <davidf0827@gmail.com>
2020-08-14 21:07:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   def self.new_by_filename(file)
     artist = file.split(" - ")[0]
     name = file.split(" - ")[1]
-    song = self.new(name)
+    song = Song.new(name)
     song.artist = artist
     song
   end
